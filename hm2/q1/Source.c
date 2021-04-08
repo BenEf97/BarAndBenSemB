acd8c75ce58b0a5118265a89b437639a8870c75a
BenEf97 <benefrath@gmail.com>
2021-04-08 23:15:29 +0300

    create trip init, progress on structs and add to end

--- hm2/q1/Source.c
+++ hm2/q1/Source.c
@@ -4,16 +4,6 @@
 #include <string.h>
 #define SIZE 80
 
-typedef struct doubleType
-{
-	struct doubleType* prev;
-	struct doubleType* next;
-}doubleType;
-
-typedef struct singleType
-{
-	struct singleType* next;
-}singleType;
 
 typedef struct
 {
@@ -32,14 +22,70 @@ typedef struct
 typedef struct Node
 {
 	int type;
-	Trip* pointer;
-	union typeNode
+	Trip* trip;
+	union Nodelist
 	{
-		doubleType *one;
-		singleType *zero;
-	};
+		struct Node{
+			struct Node *prev;
+			struct Node *nextDouble;
+		}*DoubleType;
+
+		struct Node *nextSingle;
+	}*listType;
 }Node;
 
-void AddNodeToEnd(Node* nodePtr,Trip* tripPtr) {
-
+void memoryFail()
+{
+	printf("Error! Out of memory!");
+	exit(1);
 }
+
+Node* AddNodeToEnd(Node* list,Trip* tripPtr) {
+	Node* ptr = list;
+
+	//Memory allocation
+	Node* newNode = (Node*)calloc(1, sizeof(Node));
+	if (newNode == NULL)
+		memoryFail();
+
+	newNode->trip = tripPtr;
+
+	//If the list is empty, the type is 0 because of calloc
+	if (list == NULL)
+	{
+		return newNode;
+	}
+	//For double type list
+	if (ptr->type)
+	{
+		for (; ptr->listType->DoubleType->nextDouble != NULL; ptr = ptr->listType->DoubleType->nextDouble);
+		ptr->listType->DoubleType->nextDouble = newNode;
+		return list;
+	}
+
+	//For single type list
+	else
+	{
+		for (; ptr->listType->nextSingle != NULL; ptr = ptr->listType->nextSingle);
+		ptr->listType->nextSingle = newNode;
+		return list;
+	}
+}
+
+Trip* createTrip()
+{
+	Trip* newTrip = (Trip*)calloc(1, sizeof(Trip));
+	if (newTrip == NULL)
+		memoryFail();
+	printf("Please enter destination: ");
+	fseek(stdin, 0, SEEK_END);
+	gets(newTrip->destination);
+	printf("Please enter date in DD/MM/YY format: ");
+	scanf("%d/%d/%d", newTrip->dateTrip.day, newTrip->dateTrip.month, newTrip->dateTrip.year);
+	fseek(stdin, 0, SEEK_END);
+	
+}
+
+int tripNumSearch()
+
+
