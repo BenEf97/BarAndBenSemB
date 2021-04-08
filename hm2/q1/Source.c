5dfe5c61ca8fc29a51db624886aff5cb47cd8ce2
BenEf97 <benefrath@gmail.com>
2021-04-08 11:19:26 +0300

    struct init, take a look if its ok

--- hm2/q1/Source.c
+++ hm2/q1/Source.c
@@ -2,4 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define SIZE 80
+
+typedef struct doubleType
+{
+	struct doubleType* prev;
+	struct doubleType* next;
+}doubleType;
+
+typedef struct singleType
+{
+	struct singleType* next;
+}singleType;
+
+typedef struct
+{
+	int day;
+	int month;
+	int year;
+}Date;
+
+typedef struct
+{
+	int numTrip;
+	char destination[SIZE];
+	Date dateTrip;
+}Trip;
+
+typedef struct Node
+{
+	int type;
+	Trip* pointer;
+	union typeNode
+	{
+		doubleType *one;
+		singleType *zero;
+	};
+}Node;
+
 
