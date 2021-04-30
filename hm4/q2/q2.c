20ddd6e6b17f544f0d82c5836042df2d0cb4a419
BenEf97 <78364069+BenEf97@users.noreply.github.com>
2021-04-30 16:18:28 +0300

    bugcheck

--- hm4/q2/q2.c
+++ hm4/q2/q2.c
@@ -1,4 +1,4 @@
-/*אסור לגשת באופן ישיר לנתונים!! רק עם הכנסה והוצאה, רלוונטי גם לחלק א'*/
+/*need to change the function, can't acsess directly, only with data retaree*/
 
 
 
@@ -23,87 +23,91 @@ typedef struct Queue
 	Node *tail;
 } Queue;
 
-int isEmpty(Queue *s)
+int dequeue(Queue* s) 
 {
-	if (s->head == NULL && s->tail == NULL) //&& is optional
-		return 1;
-	return 0; //there is no need for else
-}
+	Node* temp = s->head;
+	unsigned int data = temp->data;
 
-//adding to the tail, no limit and there is no need to check if empty
-void enqueue(Queue *s, unsigned int data)
-{
-	//allocating memory, 
-	//Node *newNode = (Node*)calloc(1, sizeof(Node)); //comment but if we use it then we don't need to set it on NULL
-	Node *newNode = (Node*)malloc(sizeof(Node));
-
-	//in case of memory allocation fail
-	if (!newNode)
+	if (isEmpty(s)) 
 	{
-		printf("Out of memory!\n");
-		exit(1);
+		printf("empty!");
+		return -1;
 	}
 
-	//placement of the data
-	newNode->data = data;
-	newNode->next = NULL;
+	s->head = s->head->next;
+	if (s->head == NULL)
+		s->tail = NULL;
+	
+	free(temp);
+	return data;
+}
 
-	//checking if the queue is empty
-	if (!isEmpty(s))
-	{
+void enqueue(Queue* s, unsigned int data)
+{
+	Node* newNode = (Node*)calloc(1, sizeof(Node));
+	newNode->data = data;
+
+	if (!isEmpty(s)) {
 		s->tail->next = newNode;
 		s->tail = newNode;
 		return;
 	}
-
-	//if the queue is empty then we do double placement
 	s->head = s->tail = newNode;
 }
 
-
-
-//removing the head
-unsigned int dequeue(Queue *s) //why isn't it void? we need the data value!
+//if the Queue is empty, will return TRUE, else will return FALSE
+int isEmpty(Queue* s) 
 {
-	unsigned int value = s->head->data;
-	Queue *temp = s;
+	if (s->head == NULL && s->tail == NULL)
+		return TRUE;
+	else return FALSE;
+}
 
-	//if the list is empty, will return 0
+//maayan initqueue
+void initQueue(Queue* s) 
+{
+	s->head = s->tail = NULL;
+}
+
+int sizeQueue(Queue s)
+{
+	int size=0;
+	while (isEmpty(&s))
+	{
+		dequeue(&s);
+		size++;
+	}
+	return size;
+}
+
+void rotate(Queue* s)
+{
+	unsigned int first,last;
+	Queue temp;
+
+	//checking if the queue is empty, if it does will return to main
 	if (isEmpty(s))
 	{
-		printf("The list is empty!\n");
-		return 0;
+		printf("The queue is empty!\n");
+		return;
 	}
-
-	//only one element
-	if (s->tail == s->head)
+	first=dequeue(s);
+	enqueue(s,first);
+	while (!isEmpty(s))
 	{
-		free(s->head);
-		s->head = NULL;
-		s->tail = NULL;
+		last++;
 	}
-
-	else {
-		s->head = temp->head->next;
-		free(temp->head);
-	}
-
-	return value;
+	
 }
 
-
-//gets a ptr for queue, switch between the head and the tail
-/*[1][2][3][4]
-to [4][1][2][3]
-can only use enqueue and dequeue, add to last and remove first*/
-void rotate(Queue *s)
-{
-	unsigned int last, first;
-
-}
 
 
 void main()
 {
-
+	Queue test;
+	initQueue(&test);
+	for (unsigned int num=1;num<5;num++) 
+		enqueue(&test,num);
+	printf("size is %d\n",sizeQueue(test));
+	printf("1 is empty, 0 isn't: %d",isEmpty(&test));
 }
