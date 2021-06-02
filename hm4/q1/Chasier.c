d28b4e2f1d38cb8c60d79f2fee7a1752fff68853
BenEf97 <benefrath@gmail.com>
2021-06-02 23:31:59 +0300

    cal invoice init

--- hm4/q1/Chasier.c
+++ hm4/q1/Chasier.c
@@ -1,8 +1,16 @@
+/*To do:
+-Special cases for every function
+-createInvoice- fputs doing problems
+-Debug*/
+
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define SIZE 80
+#define TRUE 1
+#define FALSE 0
 
 typedef struct groceryItem
 {
@@ -22,6 +30,7 @@ void undo(groceryList * pList);
 void CreateInvoice(groceryList * pList);
 void CalcInvoiceRowSummary();
 void allocationFail();
+void openFileFailed();
 
 void main()
 {
@@ -30,9 +39,8 @@ void main()
 	pList.listItems = (groceryItem**)calloc(1, sizeof(groceryItem));
 	pList.size = 0;
 	addGroceryItem(&pList);
-	printf("%s, %d\n", pList.listItems[0]->itemsName,pList.listItems[0]->quantity);
 	addGroceryItem(&pList);
-	printf("%s, %d\n", pList.listItems[1]->itemsName, pList.listItems[1]->quantity);
+	CreateInvoice(&pList);
 	undo(&pList);
 	undo(&pList);
 	//free(&pList.listItems);
@@ -72,6 +80,7 @@ void addGroceryItem(groceryList * pList)
 	printf("Please enter the name of the item: ");
 	fseek(stdin, 0, SEEK_END);
 	fgets(temp.itemsName, SIZE, stdin);
+	temp.itemsName[strlen(temp.itemsName)-1] = '\0';
 
 	//Input for quantity
 	printf("Please enter the quantity of the item: ");
@@ -112,7 +121,21 @@ void undo(groceryList * pList)
 	else free(pList->listItems);
 }
 
+
+//If file opening fails, the user will get output and the program will exit.
+void openFileFailed()
+{
+	printf("The file didn't open!");
+	exit(1);
+}
+
 /*Q1 c: this function gets a list and creates a recipet file for the specific list.*/
 void CreateInvoice(groceryList * pList){
-	}void CalcInvoiceRowSummary(){
-}
+	//opens a file, if it doesn't exist a new one will be created
+	FILE *pf = fopen("invoice.txt","w");	//if the opening failed	if (!pf)		openFileFailed();	//Printing the info into the file	for (int idx = 0; idx < pList->size; idx++)	{		//debug: idk but if I comment fputs i get an error		fputs(pList->listItems[idx]->itemsName,pf);		fprintf(pf, " %d %f\n", pList->listItems[idx]->quantity, pList->listItems[idx]->price);	}	//Closing the file	fclose(pf);}//Q1 d: This function summeries the item in every linevoid CalcInvoiceRowSummary(){
+	//maayan said that there is an example without another file, and we actually can create a new one
+
+
+
+	//Creating a opinter to the file
+	FILE *pf = fopen("invoice.txt", "r+");	char c;	int integer = 0, quantity, temp=TRUE;	float result = 0, pointFloat = 10;	//moving the cur to the start	fseek(pf, 0, SEEK_SET);	//my idea is a loop that is active untill we are in the end of file (EOF)	//we are looking for '\n', then we will count the numbers and create them. we know that we will find 2 spaces	while (!feof(pf))	{		//assuming the name has no numbers and digits		//if a number found		c = fgetc(pf);		while ((c <= '9' && c >= '0') || c == '.')		{			//decimal point found			if (c == '.')			{				//adding all the floating digits				while (c != '\n')				{					result += (float)c / pointFloat;					pointFloat *= 10;				}				result += (float)integer;				break;			}			integer = integer * 10 + (int)c;			fseek(pf, 1, SEEK_CUR);			c = fgetc(pf);		}		//if the quantity was calculated. Default is TRUE.		if (temp)		{			quantity = integer;			temp = FALSE;			integer = 0;		}		//reaching the end of the line		if (c == '\n')		{			result *= (float)quantity;		}		fseek(pf, 1, SEEK_CUR);	}	}
